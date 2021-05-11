cbeaaebc4eef3b77698fd89b0ae373f8ab680afd
deeje <deeje@mac.com>
2021-05-11 13:29:49 -0700

    NSPersistentContainer.performBackgroundPushTask
    convenience func to automatically name the background context, which triggers the push to CloudKit

---
+++ Source/Extensions/NSManagedContainer.swift
@@ -0,0 +1,19 @@
+//
+//  NSManagedObjectContext.swift
+//  CloudCore
+//
+//  Created by deeje cooley on 5/11/21.
+//
+
+import CoreData
+
+extension NSPersistentContainer {
+    
+    public func performBackgroundPushTask(_ block: @escaping (NSManagedObjectContext) -> Void) {
+        performBackgroundTask { moc in
+            moc.name = CloudCore.config.pushContextName
+            block(moc)
+        }
+    }
+    
+}
